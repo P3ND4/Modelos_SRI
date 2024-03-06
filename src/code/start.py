ae9abe0c2b71c1ea2807926eaba052836c8ee169
Machi-san <maxibengochea67@gmail.com>
2024-03-06 02:59:18 -0500

    visual ok

--- src/code/start.py
+++ src/code/start.py
@@ -5,14 +5,23 @@ import os
 
 #flujo del funcionamiento del modelo
 def start():
+  #feedback mientras cargamos el corpus
+  print('Loading...')
+  
+  #cargar el corpus
   docs = load()
   data_corpus = parse_corpus()
   
   while(True):
+    os.system('clear')
     query = input('Ingress your query:\n-> ')
     query_fnd = query_to_dnf(query)
     data_query = parse_query(modify_query(query_fnd))
     response = rec_docs(data_query, data_corpus, docs)
+    
+    #imprimir hasta 10 docs en pantalla
+    print('')
+    print('RESULTS')
     i = 1
     
     for element in response:
@@ -22,7 +31,8 @@ def start():
       print(f'{i}-{element[:40]}.....')
       i += 1
     
-    spacy = input('Press enter to go back')
+    print('')
+    spacy = input('Press enter to go back...')
     os.system('clear')
 
 start()
